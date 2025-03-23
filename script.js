99d63828338fb9c4f94736f956b6d0e5d5724012
TXDYSYZF <txdysyzf@gmail.com>
2025-03-24 00:51:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function showMovieDetails(description, link) {
+function showMovieDetails(description, link, linkText) {
     document.getElementById('linkBox').innerHTML = `
         <p>${description}</p>
-        <p>点击观看: <a href="${link}" target="_blank" style="color: yellow;">${link}</a></p>
+        <p><a href="${link}" target="_blank" style="color: yellow;">${linkText}</a></p>
     `;
 }
