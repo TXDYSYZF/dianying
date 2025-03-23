35e6722fd5b2d5820881a03c5e413e9d1f94a9c8
TXDYSYZF <txdysyzf@gmail.com>
2025-03-24 00:48:52 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showMovieDetails(description, link) {
-    document.getElementById('linkBox').innerHTML = `
-        <p>${description}</p>
-        <p>点击观看: <a href="${link}" target="_blank" style="color: yellow;">${link}</a></p>
-    `;
-}
