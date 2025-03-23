e56a2bd451238aaf1435ee5368fb3f615a2ef564
TXDYSYZF <txdysyzf@gmail.com>
2025-03-24 00:27:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const movies = [
-        { title: "电影 1", img: "movie1.jpg", link: "https://example.com/movie1" },
-        { title: "电影 2", img: "movie2.jpg", link: "https://example.com/movie2" },
-        { title: "电影 3", img: "movie3.jpg", link: "https://example.com/movie3" }
+        { img: "images/movie1.jpg", link: "https://example.com/movie1" },
+        { img: "images/movie2.jpg", link: "https://example.com/movie2" },
+        { img: "images/movie3.jpg", link: "https://example.com/movie3" }
     ];
 
     const container = document.getElementById("movie-container");
@@ -10,8 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
     movies.forEach(movie => {
         const movieElement = document.createElement("div");
         movieElement.classList.add("movie");
-        movieElement.innerHTML = `<img src="${movie.img}" alt="${movie.title}">`;
+
+        const img = document.createElement("img");
+        img.src = movie.img;
+
+        movieElement.appendChild(img);
         movieElement.onclick = () => window.location.href = movie.link;
+
         container.appendChild(movieElement);
     });
 });
