1d798690e8ff63941df928e1e404535387f4baba
TXDYSYZF <txdysyzf@gmail.com>
2025-03-24 00:30:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const movies = [
-        { img: "images/movie1.jpg", link: "https://example.com/movie1" },
-        { img: "images/movie2.jpg", link: "https://example.com/movie2" },
-        { img: "images/movie3.jpg", link: "https://example.com/movie3" }
-    ];
+// 电影列表（图片路径 + 对应链接）
+const movies = [
+    { image: "images/movie1.jpg", link: "https://example.com/movie1" },
+    { image: "images/movie2.jpg", link: "https://example.com/movie2" },
+    { image: "images/movie3.jpg", link: "https://example.com/movie3" }
+];
 
-    const container = document.getElementById("movie-container");
+// 动态生成电影图片
+const gallery = document.getElementById("gallery");
 
-    movies.forEach(movie => {
-        const movieElement = document.createElement("div");
-        movieElement.classList.add("movie");
-
-        const img = document.createElement("img");
-        img.src = movie.img;
-
-        movieElement.appendChild(img);
-        movieElement.onclick = () => window.location.href = movie.link;
-
-        container.appendChild(movieElement);
-    });
+movies.forEach(movie => {
+    const img = document.createElement("img");
+    img.src = movie.image;
+    img.alt = "电影";
+    img.onclick = () => showLink(movie.link);
+    gallery.appendChild(img);
 });
+
+// 显示链接
+function showLink(link) {
+    document.getElementById('linkBox').innerHTML = `电影链接: <a href="${link}" target="_blank" style="color: yellow;">${link}</a>`;
+}
