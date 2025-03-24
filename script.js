f0838bc4c82963718e929323577082f0c2d11cbb
TXDYSYZF <txdysyzf@gmail.com>
2025-03-24 23:53:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function showModal(title, link) {
+function showModal(title, links) {
     document.getElementById("modal-title").innerText = title;
-    document.getElementById("movie-link").href = link;
+    let modalLinks = document.getElementById("modal-links");
+    modalLinks.innerHTML = "";  // 清空旧链接
+
+    links.forEach(link => {
+        let a = document.createElement("a");
+        a.href = link.url;
+        a.innerText = link.title;
+        a.target = "_blank";
+        modalLinks.appendChild(a);
+    });
+
     document.getElementById("movie-modal").style.display = "flex";
 }
 
